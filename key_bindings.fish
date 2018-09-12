97cf3cfabcea5a16bab8dfddf409d2bad851b6f0
Yukito Ito <yukibukiyou@gmail.com>
2018-09-12 12:48:30 +0900

    fix

--- key_bindings.fish
+++ key_bindings.fish
@@ -1,4 +1,4 @@
-bind \x1b '__fish_fzf_z' # Ctrl-[
-# if bind -M insert >/dev/null ^/dev/null
-#     bind -M insert \x1b '__fish_fzf_z'
-# end
+bind \c[ '__fish_fzf_z' # Ctrl-[
+if bind -M insert >/dev/null ^/dev/null
+    bind -M insert \c[ '__fish_fzf_z'
+end
